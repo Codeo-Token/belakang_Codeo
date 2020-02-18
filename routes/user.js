edd54c1d9544f78c5c184e37e5749889a1e7d90d
widiawantiyesi <widiawantiyesi@gmail.com>
2020-02-18 19:41:10 +0700

    first commit

--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,7 @@ router.post("/register",addUser);
 router.post("/login",login);
 router.get("/auth",getAuth);
 router.get("/data",getAll);
-router.get("/data/_id",getOne )
+router.get("/data/:_id",getOne )
 
 
 module.exports = router;
